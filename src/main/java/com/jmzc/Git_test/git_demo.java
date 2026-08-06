79c8064212593ebe6cdf14ae5f395a7d79e1ec2b
jmzc <jmzc@jmzc.com>
2026-08-06 21:39:45 +0800

    3 commit

--- src/main/java/com/jmzc/Git_test/git_demo.java
+++ src/main/java/com/jmzc/Git_test/git_demo.java
@@ -4,5 +4,6 @@ public class git_demo {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git!");
+        System.out.println("Hello git!");
     }
 }
