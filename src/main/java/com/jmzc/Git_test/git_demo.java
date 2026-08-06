ef1af1f8585f6c778c499f8dbc76e17f0b501776
jmzc <jmzc@jmzc.com>
2026-08-06 22:02:07 +0800

    hot-fix commit1

--- src/main/java/com/jmzc/Git_test/git_demo.java
+++ src/main/java/com/jmzc/Git_test/git_demo.java
@@ -5,5 +5,6 @@ public class git_demo {
         System.out.println("Hello git!");
         System.out.println("Hello git!");
         System.out.println("Hello git!");
+        System.out.println("Hello git!");
     }
 }
