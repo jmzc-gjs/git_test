643b4d5819d28e23064ef0ac2da6e7e6a4a51b2f
jmzc-gjs <3222491353@qq.com>
2026-08-08 11:28:57 +0800

    pull commit

--- src/main/java/com/jmzc/Git_test/git_demo.java
+++ src/main/java/com/jmzc/Git_test/git_demo.java
@@ -8,5 +8,6 @@ public class git_demo {
         System.out.println("Hello git!");
         System.out.println("Hello git123!");
         System.out.println("Hello git456!");
+        System.out.println("pull!");
     }
 }
